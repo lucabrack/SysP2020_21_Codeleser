e8b2cdad3a4a5bf39e1fc44779591294c9747a1d
lucabrack <luca.brack@ost.ch>
2020-12-14 15:59:47 +0100

    created separate py file for feature calculations

---
+++ raspi/lib/contour_features.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import math
+
+def get_center_roi(roi_attr, full_img):
+    x,y,w,h = roi_attr
+    cx = x + w/2
+    cy = y + h/2
+    return cx / full_img.shape[1], cy / full_img.shape[0]
+
+def get_area_roi(roi_attr, full_img):
+    x,y,w,h = roi_attr
+    area = w*h
+    area_scaled = area / (full_img.shape[0] * full_img.shape[1])
+    return area, area_scaled
+
+def get_center(contour, roi):
+    moments = cv2.moments(contour)
+    cx = moments['m10']/moments['m00']
+    cy = moments['m01']/moments['m00']
+    return cx / roi.shape[1], cy / roi.shape[0]
+
+def get_area(contour, scale=1):
+    area = cv2.contourArea(contour)
+    return area / scale
+
+def get_perimeter(contour, scale=1):
+    perimeter = cv2.arcLength(contour, True)
+    return perimeter / scale
+
+def get_orientation(contour):
+    if len(contour) > 4:
+        _, _, angle = cv2.fitEllipse(contour)
+        return angle
+    else:
+        return 0
