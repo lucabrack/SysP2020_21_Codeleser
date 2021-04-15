01b1c466b72697e33554b5e5193da5155181d4a8
lucabrack <luca.brack@ost.ch>
2021-04-15 11:28:09 +0200

    changed info string to info list

--- raspi/image_info.py
+++ raspi/image_info.py
@@ -18,8 +18,8 @@ import lib.image_processing as img_proc
 
 ## Get all infos of every contour found
 def get_image_info(img, image_parameters, save_imgs=True, debug_folder_path='./raspi/debug/', max_saves=10):
-    # All the infos get saved in one consecutive string
-    info_str = ""
+    # All the infos get saved in one list
+    info_list = []
     
     img_gray = img_proc.grayscale(img)
     
@@ -42,62 +42,55 @@ def get_image_info(img, image_parameters, save_imgs=True, debug_folder_path='./r
         # Get Centre and Area of Region of Interest, write them to the info string
         roi_x, roi_y = get_center_roi(roi_attr, img_gray_resized)
         roi_area_scaled = get_area_roi(roi_attr, img_gray_resized)
-        info_str = build_info_str(info_str, roi_x)
-        info_str = build_info_str(info_str, roi_y)
-        info_str = build_info_str(info_str, roi_area_scaled)
+        info_list.append(roi_x)
+        info_list.append(roi_y)
+        info_list.append(roi_area_scaled)
         
         # Make ROI gray and search for contours
         roi_gray = img_proc.get_roi(img_gray, tuple([x*8 for x in roi_attr]))    
         contours, roi_bin = img_proc.get_roi_contours(roi_gray, image_parameters)
 
-        # Measure focus of ROI, get number of contours found, write them to info string 
-        info_str = build_info_str(info_str, int(img_proc.measure_focus(roi_gray)))
-        info_str = build_info_str(info_str, len(contours))
+        # Measure focus of ROI write it to info string 
+        info_list.append(int(img_proc.measure_focus(roi_gray)))
 
         # Save images for debugging purposes
         if save_imgs:
             save_img(folder_path, "02_roi", roi_gray)
             save_img(folder_path, "03_roi_binarized", roi_bin)
 
-        if 'n' in enclosure_parser:
-            del enclosure_parser['n'] # delete the 'none' item, as it serves no purpose here
-        enclose_funcs = list(enclosure_parser.values())
+        shapes = img_proc.get_shapes(contours, roi_gray)
+        info_list.append(len(shapes))
+        for shape in shapes:
+            info_list.append(shape[0])
+            print(shape[0])
+            info_list.append(shape[1][0])
+            info_list.append(shape[1][1])
 
-        # loop through all enclosing functions
-        for i, enclose_func in enumerate(enclose_funcs):
-            enc = []
-            # write the name of the function to the info string
-            info_str = build_info_str(info_str, enclose_func.__name__)
-
-            # loop through all contours found in the ROI
-            for c in contours:
-                enclosing = enclose_func(c) 
-                enc.append(enclosing)
-
-                # calculate the different features, write them to the info string
-                cx, cy = get_center(enclosing, roi_gray)
-                area = get_area(enclosing, roi_gray)
-                perimeter = get_perimeter(enclosing, roi_gray)
-                point_count = get_point_count(enclosing, None)
-                
-                info_str = build_info_str(info_str, cx)
-                info_str = build_info_str(info_str, cy)
-                info_str = build_info_str(info_str, area)
-                info_str = build_info_str(info_str, perimeter)
-                info_str = build_info_str(info_str, point_count)
-
-            # save images and info string for debugging purposes
-            if save_imgs:
-                roi_copy = img_proc.bgr(roi_gray.copy())
-                img_proc.draw_contours(roi_copy, enc)
-                save_img(folder_path, f'{i+4:02d}_roi_' + enclose_func.__name__, roi_copy)
-                save_info_str(folder_path, info_str)
+        # save images and info string for debugging purposes
+        if save_imgs:
+            roi_copy = img_proc.bgr(roi_gray.copy())
+            img_proc.draw_contours(roi_copy, contours)
+            save_img(folder_path, '04_contour', roi_copy)
+            '''
+            roi_copy = img_proc.bgr(roi_gray.copy())
+            img_proc.draw_contours(roi_copy, circle(contours))
+            save_img(folder_path, '05_circle', roi_copy)
+            roi_copy = img_proc.bgr(roi_gray.copy())
+            img_proc.draw_contours(roi_copy, box(contours))
+            save_img(folder_path, '05_rectangle', roi_copy)
+            roi_copy = img_proc.bgr(roi_gray.copy())
+            img_proc.draw_contours(roi_copy, hull(contours))
+            save_img(folder_path, '05_hull', roi_copy)
+            '''
+            save_info_list(folder_path, info_list)
 
     else: # Region of Interest was not found
-        info_str = "NothingFound"
+        info_list.append(4)
+        info_list.append(0)
+        info_list.append(4)
         print("Region of Interest not found.")
 
-    return info_str
+    return info_list
 
 
 ## Create and return image folder and delete old ones
@@ -135,31 +128,12 @@ def save_img(folder_path, img_name, img):
             print("Saving image " + full_path + " failed.")
 
 
-## Build the info string
-def build_info_str(info_str, value):
-    # All the infos get seperated with commas
-    if len(info_str) > 0:
-        info_str += ','
-
-    # all types get casted to string
-    if isinstance(value, int):
-        info_str += str(value)
-    elif isinstance(value, float):
-        info_str += f'{value:.6f}'
-    elif isinstance(value, str):
-        info_str += value
-    else:
-        print('Unhandled instance for info string: ' + str(type(value)))
-
-    return info_str
-
-
 ## Save the info string
-def save_info_str(folder_path, info_str):
+def save_info_list(folder_path, info_list):
     # if the folder path doesn't exist, nothing gets saved
     if folder_path is not None:
         with open(os.path.join(folder_path, "image_infos.txt"), 'w') as txt:
-            txt.write(info_str)
+            txt.write(str(info_list))
     else:
         print("Info String could not be saved.")
         print("Folder Path " + str(folder_path) + " does not exist.")
