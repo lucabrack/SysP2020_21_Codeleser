d3d42a30b34ab6718f32e1bd0071c667be69b8e8
lucabrack <luca.brack@ost.ch>
2021-04-27 16:36:18 +0200

    removed all led lines

--- raspi/serial_com.py
+++ raspi/serial_com.py
@@ -5,25 +5,19 @@ from gpiozero import LED
 from image_info import get_image_info
 from lib.config_reader import ConfigReader
 from lib.cam import Camera
+from time import time
 
 # Read the config file
 config = ConfigReader()
 print("Starting Camera")
 
 # Define Hardware Pins
-main_led = LED(int(config.param['hardware_parameters']['main_led_pin']))
 flash = LED(int(config.param['hardware_parameters']['flash_pin']))
-cam_led = LED(int(config.param['hardware_parameters']['cam_led_pin']))
-serial_led = LED(int(config.param['hardware_parameters']['serial_led_pin']))
-
-# Turn main LED on
-main_led.on()
 
 # Open camera
 sleep(5)
 cam = Camera(config.param['camera_parameters'])
 cam.open()
-cam_led.on()
 
 # Open serial port
 ser = serial.Serial()
@@ -33,7 +27,6 @@ ser.open()
 ser.reset_input_buffer()
 ser.reset_output_buffer()
 print("Serial openend")
-serial_led.on()
 
 def send_int_list(int_list):
     print(int_list)
@@ -68,18 +61,14 @@ while True:
 
     # Take image, process it and send the info string
     elif data_str == 'shoot':
-        cam_led.blink(0.3,0.3)
         image = cam.capture_image()
         info_list = get_image_info(image, config.param['image_parameters'], save_imgs=False)
-        cam_led.on()
         send_int_list(info_list)
 
     # Take image, process it and send the info string
     elif data_str == 'save':
-        cam_led.blink(0.3,0.3)
         image = cam.capture_image()
         info_list = get_image_info(image, config.param['image_parameters'], save_imgs=True)
-        cam_led.on()
         send_int_list(info_list)
 
     # Turn Flash on and off
@@ -101,9 +90,3 @@ while True:
 # Close the open interfaces
 cam.close()
 ser.close()
-
-# Turn all LEDs off
-main_led.off()
-flash.off()
-cam_led.off()
-serial_led.off()
